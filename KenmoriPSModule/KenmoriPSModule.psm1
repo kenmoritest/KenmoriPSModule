4f247e7d58dc4be3c62d8961d171985950a7ec20
Kengo Mori <kenmori@microsoft.com>
2019-08-02 21:58:58 +0900

    Initial Commit

---
+++ KenmoriPSModule/KenmoriPSModule.psm1
@@ -0,0 +1,87 @@
+﻿
+Add-Type -Assembly System.Web
+
+function Encode-Url
+{
+  param(
+   [parameter(Mandatory=$true, Position=0, ValueFromPipeLine=$true)]
+   $InputObject
+  )
+
+  #return [System.Web.HttpUtility]::UrlEncode($InputObject)
+  return [System.Uri]::EscapeDataString($InputObject)
+}
+
+function Decode-Url
+{
+  param(
+   [parameter(Mandatory=$true, Position=0, ValueFromPipeLine=$true)]
+   $InputObject
+  )
+  return [System.Web.HttpUtility]::UrlDecode($InputObject)
+}
+
+function Encode-Html
+{
+  param(
+   [parameter(Mandatory=$true, Position=0, ValueFromPipeLine=$true)]
+   $InputObject
+  )
+  return [System.Web.HttpUtility]::HtmlEncode($InputObject)
+}
+
+function Decode-Html
+{
+  param(
+   [parameter(Mandatory=$true, Position=0, ValueFromPipeLine=$true)]
+   $InputObject
+  )
+  return [System.Web.HttpUtility]::HtmlDecode($InputObject)
+}
+
+function Decode-SafeLinks
+{
+  param(
+   [parameter(Mandatory=$true, Position=0, ValueFromPipeLine=$true)]
+   $InputObject
+  )
+  
+  return (Parse-QueryString $InputObject)["url"]
+}
+
+function Parse-QueryString
+{
+  param(
+   [parameter(Mandatory=$true, Position=0, ValueFromPipeLine=$true)]
+   $InputObject
+  )
+  $uri = New-Object System.Uri($InputObject)
+  $ht = New-Object System.Collections.Hashtable
+  $data = [System.Web.HttpUtility]::ParseQueryString($uri.Query)
+  foreach ($key in $data.Keys)
+  {
+    $ht.Add($key, $data[$key])
+  }
+  return $ht
+}
+
+function Convert-HashToPSObject
+{
+  param(
+   [parameter(Mandatory=$true, Position=0, ValueFromPipeLine=$true)]
+   $InputObject
+  )
+
+  return New-Object PSObject -Property $InputObject
+}
+
+function Convert-HashesToPSObjects
+{
+  param(
+   [parameter(Mandatory=$true, Position=0, ValueFromPipeLine=$true)]
+   $InputObject
+  )
+  process { $InputObject |% { Convert-HashToPSObject $_ } }
+
+}
+
---
+++ Readme.md
@@ -0,0 +1,28 @@
+# Kenmori PSModule
+
+## Installation Step
+Copy the KenmoriPSModule folder to the following local folder path.
+
+C:\Program Files\WindowsPowerShell\Modules
+
+## Commands
+These are the commands that are installed by this PS Module.
+
+### Encode-Url
+
+### Decode-Url
+
+### Encode-Html
+
+### Decode-Html
+
+### Parse-QueryString
+
+### Decode-SafeLinks
+
+### Convert-HashToPSObject
+
+### Convert-HashesToPSObjects
+
+ Example)
+  $hasharray | Convert-HashesToPSObjects | Out-GridView
